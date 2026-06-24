9bdefab1024be6bc7417172c1d1ac432bd6e576c
EricCatayas <catayasericjay@gmail.com>
2026-06-24 12:26:17 +0800

    update: new auth user placeholder id

--- supabase/migrations/0003_new_auth_user.sql
+++ supabase/migrations/0003_new_auth_user.sql
@@ -1,8 +1,8 @@
 -- New auth user for testing purposes in postman
 insert into users (id, email)
-values ('442cf39c-9014-4642-88ef-75a83b57d938', 'alice-auth@acme.test')
+values ('new-auth-id', 'alice-auth@acme.test')
 on conflict (id) do nothing;
 
 insert into memberships (user_id, group_id)
-values ('442cf39c-9014-4642-88ef-75a83b57d938', '11111111-1111-1111-1111-111111111111')
+values ('new-auth-id', '11111111-1111-1111-1111-111111111111')
 on conflict (user_id, group_id) do nothing;
